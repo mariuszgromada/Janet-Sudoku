d7d544362508a205ab3e1aeb0a3df7900d7d76c1
Mariusz Gromada <mariusz.gromada@gmail.com>
2016-03-16 00:22:35 +0100

    Change in file name

--- CURRENT/java/src/org/mariuszgromada/math/janetsudoku/regtests/RegTestsApir.java
+++ CURRENT/java/src/org/mariuszgromada/math/janetsudoku/regtests/RegTestsApir.java
@@ -68,7 +68,7 @@ import org.mariuszgromada.math.janetsudoku.SudokuStore;
  *
  * @see SudokuSolver
  */
-public class RegTestsApi {
+public class RegTestsApir {
 	/**
 	 * Runs SudokuStore regression tests with default number of threads.
 	 * @return    Number of tests with errors.
